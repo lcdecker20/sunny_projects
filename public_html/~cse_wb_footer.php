256e10c20417272a7eea39807b3e5b429f5f4d39
Leslie Decker <leslie.decker@my.simpson.edu>
2018-10-23 09:18:17 -0500

    change to php

---
+++ public_html/~cse_wb_footer.php
@@ -0,0 +1,3 @@
+ <footer id="foot"><h6><em> This is a publication of Sunny Island Resorts and is owned by Sunny Island Resorts<!-- heyo prefessor craven -->
+  and copies of this not sanctioned by Sunny Island Resorts are illegal and subject to criminal fines and punishment.</em></h6>
+  </footer>
